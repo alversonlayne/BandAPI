ad6b23878d4f7709456d81f81acb372b5248b8d4
Alverson Layne <alverson.layne2@gmail.com>
2020-05-19 09:34:11 -0400

    Created CRUD operations in BandAlbumRepository

--- Services/IBandAlbumRepository.cs
+++ Services/IBandAlbumRepository.cs
@@ -9,8 +9,8 @@ namespace BandAPI.Services
     public interface IBandAlbumRepository
     {
         IEnumerable<Album> GetAlbums(Guid bandId);
-        Album GetAlbum(Guid BandId, Guid albumId);
-        void AddAlbum(Guid BandId, Album album);
+        Album GetAlbum(Guid bandId, Guid albumId);
+        void AddAlbum(Guid bandId, Album album);
         void UpdateAlbum(Album album);
         void DeleteAlbum(Album album);
 
