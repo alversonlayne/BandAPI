1035cc3f35f0b65c3f111d1cb6c9bb6a89c7ed7d
Alverson Layne <alverson.layne2@gmail.com>
2020-05-25 21:12:49 -0400

    Created Band Data Transfer Object class with "FoundedYearsAgo" string property

---
+++ Models/BandDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Models
+{
+    public class BandDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string FoundedYearsAgo { get; set; }
+        public string MainGenre { get; set; }
+    }
+}
